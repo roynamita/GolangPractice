5aabb89a55aa0cb98fd010c318c22d8a82106bb7
Namita Roy <roynamita253@gmail.com>
2020-01-31 14:30:39 +0530

    Initial commit

---
+++ deck.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValues{
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards 
+}
+
+func deal(cards deck, number int) (deck, deck)  {
+
+	return cards[:number], cards[number:]
+	
+}
+
+func (d deck) print() {
+	for i, card := range d{
+		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string{
+	return strings.Join([]string(d), ",")
+}
+
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main()  {
+	cards := newDeck()
+	fmt.Println(cards.toString())
+}
+
+func newCard() string{
+	return "Five of Spade"
+}
